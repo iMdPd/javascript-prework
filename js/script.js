925511e3318311efe50aa05c6558ff09bd9970c0
Mariusz Politowicz <politowiczmar@gmail.com>
2022-11-29 20:45:58 +0100

    Add computer move functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-let computerMove = "kamień";
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz"
-);
+console.log("Wylosowana liczba to: " + randomNumber);
 
-let playerMove = "Papier";
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "Kamień";
+} else if (randomNumber == 2) {
+  computerMove = "Papier";
+} else if (randomNumber == 3) {
+  computerMove = "Nożyce";
+}
+
+printMessage("Mój ruch to: " + computerMove);
