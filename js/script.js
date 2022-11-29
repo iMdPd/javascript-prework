46116052abd3746e5bb0b89a67524c35417b26d1
Mariusz Politowicz <politowiczmar@gmail.com>
2022-11-29 18:33:19 +0100

    Fixed printMessage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let computerMove = "kamień";
 
 printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruchto papier, to wygrywasz"
+  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz"
 );
 
 let playerMove = "Papier";
