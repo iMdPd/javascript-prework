d5c770460fa5cf1fc142bc20d587ebea38599f66
Mariusz Politowicz <politowiczmar@gmail.com>
2022-11-29 20:52:57 +0100

    Add player move functions

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ if (randomNumber == 1) {
 }
 
 printMessage("Mój ruch to: " + computerMove);
+
+let playerInput = prompt("Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.");
+
+console.log("Wybrana przez gracza liczba to: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == 1) {
+  playerMove = "Kamień";
+} else if (playerInput == 2) {
+  playerMove = "Papier";
+} else if (playerInput == 3) {
+  playerMove = "Nożyce";
+}
+
+printMessage("Gracz wybrał: " + playerMove);
